f59d2866d2764761a2fb89fa3f1e865cef63ad49
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-14 18:21:38 -0500

    Add more tests

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -1,8 +1,7 @@
-import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
-import { PersonComponent } from './person.component';
+import { Person, PersonComponent } from './person.component';
 
 fdescribe('PersonComponent', () => {
   let component: PersonComponent;
@@ -15,6 +14,8 @@ fdescribe('PersonComponent', () => {
 
     fixture = TestBed.createComponent(PersonComponent);
     component = fixture.componentInstance;
+    const person: Person = { name: 'Sebas', height: 1.75, weight: 75 };
+    component.person = person;
     fixture.detectChanges();
   });
 
@@ -22,17 +23,68 @@ fdescribe('PersonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it("shout have an h3 element with 'Hola, PersonComponent'", () => {
+  it('should have the name "Sebas"', () => {
     const personDebug = fixture.debugElement;
     const h3Debug = personDebug.query(By.css('h3'));
     const h3: HTMLElement = h3Debug.nativeElement;
-    expect(h3.textContent).toBe('Hola, PersonComponent');
+
+    fixture.detectChanges();
+
+    expect(h3.textContent).toContain('Sebas');
   });
 
-  it("should have a p element with 'Soy un parrafo'", () => {
-    const personDebug: DebugElement = fixture.debugElement;
-    const pDebug: DebugElement = personDebug.query(By.css('p'));
-    const personElement: HTMLElement = pDebug.nativeElement;
-    expect(personElement?.textContent).toBe('Soy un parrafo');
+  it('should have the height "1.75"', () => {
+    const personDebug = fixture.debugElement;
+    const pDebug = personDebug.query(By.css('p#altura'));
+    const p: HTMLElement = pDebug.nativeElement;
+
+    fixture.detectChanges();
+
+    expect(p.textContent).toContain('1.75');
+  });
+
+  it('should have the weight "75"', () => {
+    const personDebug = fixture.debugElement;
+    const pDebug = personDebug.query(By.css('p#peso'));
+    const p: HTMLElement = pDebug.nativeElement;
+
+    fixture.detectChanges();
+
+    expect(p.textContent).toContain('75');
+  });
+
+  it('should have the BMI "24.49"', () => {
+    const bmi = component.computeBMI(
+      component.person.height,
+      component.person.weight
+    );
+    expect(bmi).toBe('Normal weight');
+  });
+
+  it('should have the BMI status "Normal weight" in the button', () => {
+    const personDebug = fixture.debugElement;
+    const buttonDebug = personDebug.query(By.css('button'));
+    const button: HTMLButtonElement = buttonDebug.nativeElement;
+
+    buttonDebug.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(button.textContent).toContain('Normal weight');
+  });
+
+  it('should return a person when clicked', () => {
+    const personDebug = fixture.debugElement;
+    const buttonDebug = personDebug.query(By.css('button#chooser'));
+    const expectedPerson: Person = { name: 'Sebas', height: 1.75, weight: 75 };
+    let selectedPerson: Person | undefined;
+
+    component.onSelected.subscribe((person) => {
+      selectedPerson = person;
+    });
+
+    buttonDebug.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(selectedPerson).toEqual(expectedPerson);
   });
 });
--- src/app/components/person/person.component.ts
+++ src/app/components/person/person.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+export interface Person {
+  name: string;
+  height: number;
+  weight: number;
+}
 
 @Component({
   selector: 'app-person',
@@ -6,5 +12,41 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./person.component.scss'],
 })
 export class PersonComponent {
-  @Input() person!: { name: string; height: number };
+  @Input() person!: Person;
+  @Output() onSelected = new EventEmitter<Person>();
+
+  bmiStatus:
+    | 'Underweight'
+    | 'Normal weight'
+    | 'Overweight'
+    | 'Obesity'
+    | 'Unknown'
+    | '' = '';
+
+  handleClick() {
+    this.bmiStatus = this.computeBMI(this.person.height, this.person.weight);
+  }
+
+  handleChoose() {
+    this.onSelected.emit(this.person);
+  }
+
+  computeBMI(height: number, weight: number) {
+    const bmi = weight / (height * height);
+
+    if (bmi < 18.5) {
+      return 'Underweight';
+    }
+    if (bmi >= 18.5 && bmi <= 24.9) {
+      return 'Normal weight';
+    }
+    if (bmi >= 25 && bmi <= 29.9) {
+      return 'Overweight';
+    }
+    if (bmi >= 30) {
+      return 'Obesity';
+    }
+
+    return 'Unknown';
+  }
 }
