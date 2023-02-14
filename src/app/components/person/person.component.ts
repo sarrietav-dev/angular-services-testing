6d2179260d0a8bf92c775602982132caf7534cdc
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-02-13 19:40:36 -0500

    Add inputs to the element

--- src/app/components/person/person.component.ts
+++ src/app/components/person/person.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
-  styleUrls: ['./person.component.scss']
+  styleUrls: ['./person.component.scss'],
 })
 export class PersonComponent {
-
+  @Input() person!: { name: string; height: number };
 }
